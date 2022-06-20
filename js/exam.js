66f3513218ea8d46e4caeb1a1a9772e1b8999fd2
M. Nurul Abser <absern46@gmail.com>
2022-06-20 11:13:35 +0600

    20th

---
+++ js/exam.js
@@ -0,0 +1,5 @@
+// document.getElementById('subjects').addEventListener('click', function (event){
+//     console.log('second-item clicked');
+//     alert('proceed');
+//     // window.location.href = 'exam_details.html'
+// })
