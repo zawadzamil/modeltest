c6043890cdd3ed6033d7422299b60a32a0fe6b26
M. Nurul Abser <absern46@gmail.com>
2022-06-22 20:17:08 +0600

    25th

--- js/set-test.js
+++ js/set-test.js
@@ -1,9 +1,17 @@
 
 // set time max linit
-var numElement = document.getElementById('time');
-numElement.addEventListener('change', Max);
-numElement.addEventListener('input', Max);
-numElement.addEventListener('keyup', Max);
+var numElementMax = document.getElementById('time-input');
+numElementMax.addEventListener('change', Max);
+numElementMax.addEventListener('input', Max);
+numElementMax.addEventListener('keyup', Max);
 function Max() {
    if (this.max) this.value = Math.min(parseInt(this.max), parseInt(this.value) || 0);
+}
+// set time min linit
+var numElementMin = document.getElementById('time-input');
+numElementMin.addEventListener('change', Min);
+numElementMin.addEventListener('input', Min);
+numElementMin.addEventListener('keyup', Min);
+function Min() {
+   if (this.Min) this.value = Math.min(parseInt(this.Min), parseInt(this.value) || 0);
 }
